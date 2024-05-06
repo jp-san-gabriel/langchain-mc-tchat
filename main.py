d8cbd726fcae1cb80eef939e732dd10539de92a0
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-05-07 00:24:47 +0800

    Use summarized chat history for context memory

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(verbose=True)
 
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"),
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory("messages.json"),
     memory_key="messages",
-    return_messages=True
+    return_messages=True,
+    llm=chat,
 )
 
 
@@ -27,6 +28,7 @@ chain = LLMChain(
     llm=chat,
     prompt=prompt,
     memory=memory,
+    verbose=True,
 )
 
 while True:
